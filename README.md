6794644c85b37551e06ebb7335494cec47fac118
Bak Gergely János <16374863+weyh@users.noreply.github.com>
2020-07-19 20:54:23 +0200

    input blocking

--- README.md
+++ README.md
@@ -4,3 +4,12 @@ The code (as many similar), based off of [Napalm's original](http://www.rohitab.
 However, many (if any) do not implement **multi-monitor support**, which ruins the effect. This version does. Plus, I added some **args** too.
 
 ![melt.png](Imgs/melt.png)
+
+### Args:
+
+ - Disable user input: `--disable_input` (ctrl+alt+del still works)
+ - Sleep time before visual effect (ms): `-t`, `--time` Example: `-t 2000`, `--time=2000`
+ 
+### License
+
+This project is licensed under the MIT License - see the [MIT License](LICENSE) file for details.
--- screen-melter/screen-melter.cpp
+++ screen-melter/screen-melter.cpp
@@ -11,10 +11,26 @@
 
 #include "argh.h"
 
+bool disableInput = false;
 int	meltWidth = 150,
 	meltHeight = 15,
 	interval = 1;
 int screenX, screenY, screenW, screenH;
+HHOOK keyboardHhook;
+
+LRESULT CALLBACK KeyboardHook(int code, WPARAM wParam, LPARAM lParam)
+{
+	KBDLLHOOKSTRUCT* kbd = (KBDLLHOOKSTRUCT*)lParam;
+
+	if(code < 0
+	   || (kbd->flags & 0x10) // ignore injected events
+	   ) return CallNextHookEx(keyboardHhook, code, wParam, lParam);
+
+	if(WM_KEYDOWN == wParam)
+		Debug::WriteLog("WM_KEYDOWN");
+
+	return 1; // by default swallow the keys
+}
 
 LRESULT WINAPI MelterProc(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam)
 {
@@ -47,6 +63,9 @@ LRESULT WINAPI MelterProc(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam)
 			}
 			return 0;
 		case WM_CLOSE:
+			#ifndef _DEBUG
+			return 0; // disable closing
+			#endif
 		case WM_DESTROY:
 			{
 				KillTimer(hWnd, 0);
@@ -61,7 +80,7 @@ LRESULT WINAPI MelterProc(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam)
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrev, LPSTR lpCmdLine, int nShowCmd)
 {
 	argh::parser cmdl;
-	cmdl.add_params({ "-t", "--time" });
+	cmdl.add_params({"-t", "--time"});
 	cmdl.parse(__argc, __argv);
 
 	std::string t = cmdl("t").str();
@@ -73,7 +92,10 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrev, LPSTR lpCmdLine, int 
 	if(!_t.empty())
 		time = std::stoi(_t);
 
-	WriteLog(std::to_string(time).c_str());
+	disableInput = cmdl[{"--disable_input"}];
+
+	Debug::WriteLog("sleep_for: " + std::to_string(time));
+	Debug::WriteLog("disable_input: " + BoolToString(disableInput));
 
 	std::this_thread::sleep_for(std::chrono::milliseconds(time));
 
@@ -85,7 +107,7 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrev, LPSTR lpCmdLine, int 
 	screenH = GetSystemMetrics(SM_CYVIRTUALSCREEN);// d |----------------------|
 
 	WNDCLASS wndClass = {0, MelterProc, 0, 0, hInstance,
-		LoadIcon(NULL, IDI_APPLICATION), LoadCursor(NULL, IDC_ARROW),
+		LoadIcon(NULL, IDI_APPLICATION), LoadCursor(NULL, NULL),
 		(HBRUSH)(COLOR_BACKGROUND + 1), 0, L"Melter"};
 
 	if(!RegisterClass(&wndClass))
@@ -94,6 +116,11 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrev, LPSTR lpCmdLine, int 
 	HWND hWnd = CreateWindowA("Melter", NULL, WS_POPUP, screenX, screenY, screenW, screenH, HWND_DESKTOP, NULL, hInstance, NULL);
 	if(!hWnd)
 		return MessageBoxA(HWND_DESKTOP, "Cannot create window!", NULL, MB_ICONERROR | MB_OK);
+
+	#ifndef _DEBUG
+	if(disableInput)
+		keyboardHhook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardHook, hInstance, 0);
+	#endif
 
 	srand(GetTickCount());
 	MSG Msg = {0};
@@ -104,8 +131,15 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrev, LPSTR lpCmdLine, int 
 			TranslateMessage(&Msg);
 			DispatchMessage(&Msg);
 		}
+		#ifdef _DEBUG
 		if(GetAsyncKeyState(VK_ESCAPE) & 0x8000)
 			DestroyWindow(hWnd);
+		#endif
+
+		#ifndef _DEBUG
+		if(disableInput)
+			SetCursorPos(0, 0);
+		#endif
 	}
 	return 0;
 }
